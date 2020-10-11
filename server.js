7550b9a8c18ba669f703c26d6b9babec042e8446
Khusharth <khusharth19@gmail.com>
2020-10-11 06:18:57 +0530

    :heavy_plus_sign: Adding mongoose

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
-dotenv.config({ path: './config.env' }); // use it before requiring app
-
 const app = require('./app');
 
-const port = process.env.PORT || 3000;
+dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// connect method returns a promise
+mongoose
+  .connect(DB, {
+    // To deal with some depreciation warnings
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }) // promise gets access to the connection object
+  .then(() => console.log('DB connection successful'));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
